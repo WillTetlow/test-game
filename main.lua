36c4ac9c0ddd7bf834043e5b82304f3b01efb6e2
Will Tetlow <will.tetlow@gmail.com>
2025-05-12 19:05:50 +0100

    Added tileset and map

--- main.lua
+++ main.lua
@@ -1,6 +1,14 @@
 function love.load()
+    camera = require 'libraries/camera'
+    cam = camera()
+
+
     anim8 = require 'libraries/anim8'
     love.graphics.setDefaultFilter("nearest", "nearest")
+
+    sti = require 'libraries/sti'
+
+    gameMap = sti('maps/test-map.lua')
 
     player = {} --curly brackets for tables --
     player.x = 400
@@ -51,9 +59,37 @@ function love.update(dt)
 
     player.anim:update(dt)
 
+    cam:lookAt(player.x, player.y)
+    
+    local w = love.graphics.getWidth()
+    local h = love.graphics.getHeight()
+
+    if cam.x < w/2 then
+        cam.x = w/2
+    end
+
+    if cam.y < h/2 then
+        cam.y = h/2
+    end
+
+    local mapW = gameMap.width * gameMap.tilewidth
+    local mapH = gameMap.height * gameMap.tileheight
+
+    if cam.x > (mapW - w/2) then
+        cam.x = (mapW - w/2)
+    end
+
+    if cam.y > (mapH - h/2) then
+        cam.y = (mapH - h/2)
+    end
+
+
 end
 
 function love.draw()
-   love.graphics.draw(background, 0, 0, 0, 3.5)
-   player.anim:draw(player.spriteSheet, player.x, player.y, nil, 5)
+    cam:attach()
+        gameMap:drawLayer(gameMap.layers["Ground"])
+        gameMap:drawLayer(gameMap.layers["Trees"])
+        player.anim:draw(player.spriteSheet, player.x, player.y, nil, 6, nil, 6, 9)
+    cam:detach()
 end
